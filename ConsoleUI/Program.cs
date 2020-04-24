ca3fe83950880f2aa0f497cf5a932937b6561d8e
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:05:39 -0700

    InitialCommit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                SimpleMethod();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static void SimpleMethod()
+        {
+            throw new InvalidOperationException("You should not be calling the simple method.");
+        }
+    }
+}
